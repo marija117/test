bd8d63b50a1df1c5b6a1e4f15f538dc7f2a09ba6
marija <kuveljic117@gmail.com>
2021-04-25 18:53:41 +0200

    Fill seeder with job adverts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+JobAdvert.create([
+    {
+      title: "Developer",
+      description: "A front-end developer specializes in building the front end, or client-side, of a web appkication.",
+      employer: "Vennage",
+      employer_email: "info@vennage.com",
+      category: "IT",
+      expiration_date: "01.06.2021".
+    },
+    {
+        title: "Project Manager",
+        description: "Responsible for planning and overseeing projects to ensure they are completed in a timely fashion and within budget.",
+        employer: "IBM",
+        employer_email: "info@ibm.com",
+        category: "IT",
+        expiration_date: "07.06.2021".
+    },
+    {
+        title: " React Developer",
+        description: "Build new features and improve existing ones of our client application and website.",
+        employer: "Marriott",
+        employer_email: "info@marriot.com",
+        category: "IT",
+        expiration_date: "17.07.2021".
+    },
+    {
+        title: "Bartender",
+        description: "Person with passion for service and passion for creating exceptional guest experience.",
+        employer: "Dallas Restaurant",
+        employer_email: "info@dallas.com",
+        category: "Catering",
+        expiration_date: "25.05.2021".
+    },
+    {
+        title: "Chef",
+        description: "Preparation of traditional and international cuisine.",
+        employer: "Mademoiselle",
+        employer_email: "info@mademoiselle.com",
+        category: "Catering",
+        expiration_date: "25.06.2021".
+    }
+  ])
