9ef7c07f8c5e3e5375b95ca2ce64cce1c622d345
marija <kuveljic117@gmail.com>
2021-04-30 14:58:26 +0200

    Modify JobApplicationController to send mail to the applicant after appyling for a job.

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,11 +1,13 @@
 class JobApplicationsController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :set_job_application, only: [:show]
+    before_action :set_job_advert, only: [:create]
 
     def create
-        @job_application = JobApplication.new(job_application_params)
+        @job_application = @job_advert.job_applications.create(job_application_params)
     
         if @job_application.save
+            JobApplicationMailer.with(job_application: @job_application).new_job_application_email.deliver_later
             redirect_to @job_application
         else
             render :new
@@ -13,7 +15,6 @@ class JobApplicationsController < ApplicationController
     end
 
     def show
-        @job_application = JobApplication.find(params[:id])
     end
 
     def new
@@ -26,7 +27,11 @@ class JobApplicationsController < ApplicationController
         @job_application = JobApplication.find(params[:id]) if params[:id]
     end
 
+    def set_job_advert
+        @job_advert = JobAdvert.find(params[:job_advert_id]) if params[:job_advert_id]
+    end
+
     def job_application_params
-        params.require(:job_application).permit(:applicant_full_name, :date_of_birth, :email, :phone_number, :address, :qualification)
+        params.require(:job_application).permit(:job_advert_id, :applicant_full_name, :date_of_birth, :email, :phone_number, :address, :qualification)
     end
 end
--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -1,7 +1,7 @@
 class JobApplicationMailer < ApplicationMailer
     def new_job_application_email
         @job_application = params[:job_application]
-    
-        mail(to: "from@example.com", subject: "You got a new application for job!")
-      end
+
+        mail(to: @job_application.email, subject: "You have successfully applied for the job!")
+    end
 end
