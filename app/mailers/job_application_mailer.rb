52ff5ce469ed5abc976c49a6655d21be6b9ccfc5
marija <kuveljic117@gmail.com>
2021-04-30 18:06:26 +0200

    Modify content of job application mail.

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -1,7 +1,7 @@
 class JobApplicationMailer < ApplicationMailer
     def new_job_application_email
         @job_application = params[:job_application]
-
-        mail(to: @job_application.email, subject: "You have successfully applied for the job!")
+        
+        mail(to: @job_application.job_advert.employer_email, subject: "You got a new application for the job!")
     end
 end
