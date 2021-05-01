838574ed5c6063672a5102a319d026dacc54a117
marija <kuveljic117@gmail.com>
2021-05-01 20:23:21 +0200

    Add model validations.

--- app/models/job_advert.rb
+++ app/models/job_advert.rb
@@ -1,3 +1,10 @@
 class JobAdvert < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+    validates :employer, presence: true
+    validates :employer_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :category, presence: true
+    validates :expiration_date, presence: true
+
     has_many :job_applications, dependent: :destroy
 end
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,3 +1,11 @@
 class JobApplication < ApplicationRecord
+    validates :applicant_full_name, presence: true
+    validates :date_of_birth, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :phone_number,   :presence => {:message => 'Phone number can not contain letters!'},
+    :numericality => true, :length => { :minimum => 10, :maximum => 15 }
+    validates :address, presence: true
+    validates :qualification, presence: true
+
     belongs_to :job_advert
 end
